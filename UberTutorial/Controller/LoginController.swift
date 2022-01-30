532d4f145643aa78fabec72c1c132ab584e87504
James Zhang <liulfbaby@gmail.com>
2022-01-31 00:31:22 +0800

    replace a root view controller, and set a simple red background color.

--- UberTutorial/Controller/LoginController.swift
+++ UberTutorial/Controller/LoginController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LoginController: UIViewController {
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
