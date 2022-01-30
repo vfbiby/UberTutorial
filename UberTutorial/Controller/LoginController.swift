5043ee4f357413ef94d1a725197c4554959b810e
James Zhang <liulfbaby@gmail.com>
2022-01-31 01:05:27 +0800

    add a "uber" text label and light the status bar font

--- UberTutorial/Controller/LoginController.swift
+++ UberTutorial/Controller/LoginController.swift
@@ -11,14 +11,30 @@ class LoginController: UIViewController {
     
     //MARK: - Properties
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
     }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        .lightContent
+    }
 
 }
 
