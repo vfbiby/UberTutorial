9e63873fbba46ece24c3a74c19aeb9021b6d13c9
James Zhang <liulfbaby@gmail.com>
2022-02-01 22:47:01 +0800

    add login control for email.

--- UberTutorial/Controller/LoginController.swift
+++ UberTutorial/Controller/LoginController.swift
@@ -19,6 +19,37 @@ class LoginController: UIViewController {
         return label
     }()
     
+    private lazy var emailContainerView: UIView = {
+        let view = UIView()
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x")
+        imageView.alpha = 0.87
+        
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+        
+        view.addSubview(emailTextfield)
+        emailTextfield.centerY(inView: view)
+        emailTextfield.anchor(left: imageView.rightAnchor, paddingLeft: 8)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        view.addSubview(separatorView)
+        separatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingLeft: 8, height: 0.75 )
+        return view
+    }()
+    
+    private let emailTextfield: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = .none
+        tf.font = UIFont.systemFont(ofSize: 16)
+        tf.textColor = .white
+        tf.keyboardAppearance = .dark
+        tf.attributedPlaceholder  = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        return tf
+    }()
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -29,6 +60,8 @@ class LoginController: UIViewController {
         view.addSubview(titleLabel)
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
         titleLabel.centerX(inView: view)
+        view.addSubview(emailContainerView)
+        emailContainerView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle{
