c17339246de154a01d2e339e09e04c2e9c49ff33
James Zhang <liulfbaby@gmail.com>
2022-02-09 18:20:36 +0800

    Polyline to pickup location & ride action config.

--- UberTutorial/View/RideActionView.swift
+++ UberTutorial/View/RideActionView.swift
@@ -12,6 +12,39 @@ protocol RideActionViewDelegate: AnyObject {
     func uploadTrip(_ view: RideActionView)
 }
 
+enum RideActionViewConfiguration{
+    case requestRide
+    case tripAccepted
+    case pickupPassenger
+    case tripInProgress
+    
+    init() {
+        self = .requestRide
+    }
+}
+
+enum ButtonAction: CustomStringConvertible {
+    case requestRide
+    case cancel
+    case getDirections
+    case pickup
+    case dropOff
+    
+    var description: String {
+        switch self {
+        case .requestRide: return "CONFIRM UBERX"
+        case .cancel: return "CANCEL RIDE"
+        case .getDirections: return "GET DIRECTIONS"
+        case .pickup: return "PICKUP PASSENGER"
+        case .dropOff: return "DROP OFF PASSENGER"
+        }
+    }
+    
+    init() {
+        self = .requestRide
+    }
+}
+
 class RideActionView: UIView {
     
     // MARK: - Properties
@@ -23,6 +56,8 @@ class RideActionView: UIView {
         }
     }
     
+    var config = RideActionViewConfiguration()
+    var buttonAction = ButtonAction()
     weak var delegate:RideActionViewDelegate?
     
     private let titleLable: UILabel = {
@@ -116,7 +151,15 @@ class RideActionView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Selectors
+    
     @objc func actionButtonPressed(){
         delegate?.uploadTrip(self)
+    }
+    
+    // MARK: - Help Functions
+    
+    func configureUI(withConfig config: RideActionViewConfiguration){
+        
     }
 }
